27781fea64afcca17c248ad4a87d79e288d941f1
Artem Danko <danko5012@gmail.com>
2024-04-07 18:57:33 +0300

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { UsersComponent } from "./pages/users/users.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { AppRoutes } from "../app/utils/constant";
+import { ForbiddenComponent } from "./pages/forbidden/forbidden.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: AppRoutes.Main, redirectTo: AppRoutes.Users, pathMatch: "full" },
+  { path: AppRoutes.Users, component: UsersComponent },
+
+  { path: AppRoutes.Forbidden, component: ForbiddenComponent },
+  { path: AppRoutes.NotFound, component: NotFoundComponent },
+
+  { path: "**", redirectTo: AppRoutes.NotFound },
+];
---
+++ src/app/utils/constant.ts
@@ -0,0 +1,6 @@
+export const AppRoutes = {
+  Main: '',
+  Users: 'users',
+  Forbidden: '403',
+  NotFound: '404',
+} as const
