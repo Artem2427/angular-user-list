03ea102778c64f167a6a2397a2009cc4e3ed642a
Artem Danko <danko5012@gmail.com>
2024-04-08 07:50:28 +0300

    feat: add modal component

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonComponent } from '@shared/components/button/button.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
+  private router = inject(Router);
 
+  public handleGoBack() {
+    this.router.navigate(['./'])
+  }
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,16 +1,18 @@
-import { Component, inject } from '@angular/core';
+import { Component, TemplateRef, ViewChild, ViewContainerRef, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from '@shared/components/button/button.component';
 import { InputComponent } from '@shared/components/input/input.component';
+import { ModalComponent } from '@shared/components/modal/modal.component';
 import { SelectComponent } from '@shared/components/select/select.component';
 import { ControlValueAccessorDirective } from '@shared/directives/control-value-accessor.directive';
 import { FormUtilitiesService } from '@shared/services/form-utilities.service';
+import { ModalService } from '@shared/services/modal.service';
 import { ToastService, toastState } from '@shared/services/toast.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [ButtonComponent, InputComponent, SelectComponent, ControlValueAccessorDirective, ReactiveFormsModule],
+  imports: [ButtonComponent, InputComponent, SelectComponent, ModalComponent, ControlValueAccessorDirective, ReactiveFormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
@@ -19,6 +21,8 @@ export class UsersComponent {
   protected formUtilities = inject(FormUtilitiesService);
 
   private toastService = inject(ToastService);
+
+  private modalService = inject(ModalService)
 
   formRegister!: FormGroup;
 
@@ -36,5 +40,14 @@ export class UsersComponent {
 
   handleButtonClick() {
     this.toastService.showToast(toastState.error, 'Success message');
+  }
+
+
+  openModal(modalTemplate: TemplateRef<any>) {
+    this.modalService
+      .open(modalTemplate, { size: 'lg', title: 'Foo' })
+      .subscribe((action) => {
+        console.log('modalAction', action);
+      });
   }
 }
